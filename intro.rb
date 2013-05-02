8e2161222ba5eda7d6499eeed461e9b9f5ecbde7
Leo <leonid.inbox@gmail.com>
2013-05-03 01:09:22 +0400

    5

--- intro.rb
+++ intro.rb
@@ -35,41 +35,79 @@ module Root
       if @context.size > 1 &&
           (@context[-1][:time] - @context[-2][:time] > 0.05.seconds) &&
           @context.map{|e| e[:char]}.join != ''
+        system 'clear'
+        p @context
         @command = @context.map{|e| e[:char]}.join
         @command = @command.split(' ').map(&:downcase).join(' ')
+        @command = @command[-1] if @command[0] == "\x7F"
         @context = []
         puts "Context: #{@command}"
         ($ninja=1; break) if @command == 'ninja'
-        begin
-          case
-          when c=Basic::Table[@command]
-            c.call
-          else
-            puts "no command #{@command}"
-          end
-        #rescue StandardError => e
-          #puts "Hey, i have no command #{e}, care to voice-input?"
-        end
       end
     end
   end
 
   module Basic
     extend self
-    Table={
-      'browser' => ->{`google-chrome`},
-      'greet'     => ->{p "notify-send 'ohaaaaaaai!'"},
-      'greet me'  => ->{`notify-send ohaaaaaaai`}
-    }
+
+    def process command
+      case command
+      when 'browser' then `google-chrome`
+      when 'greet'   then `notify-send ohaaaaaaai`
+
+      when 'ash'   then `xte 'key a'`
+      when 'ash'   then `xte 'key b'`
+      when 'ash'   then `xte 'key c'`
+      when 'ash'   then `xte 'key d'`
+      when 'ash'   then `xte 'key e'`
+      when 'ash'   then `xte 'key f'`
+      when 'ash'   then `xte 'key g'`
+      when 'ash'   then `xte 'key h'`
+      when 'ash'   then `xte 'key i'`
+      when 'ash'   then `xte 'key j'`
+      when 'ash'   then `xte 'key k'`
+      when 'ash'   then `xte 'key l'`
+      when 'ash'   then `xte 'key m'`
+      when 'ash'   then `xte 'key n'`
+      when 'ash'   then `xte 'key o'`
+      when 'ash'   then `xte 'key p'`
+      when 'ash'   then `xte 'key q'`
+      when 'ash'   then `xte 'key r'`
+      when 'ash'   then `xte 'key s'`
+      when 'ash'   then `xte 'key t'`
+      when 'ash'   then `xte 'key u'`
+      when 'ash'   then `xte 'key v'`
+      when 'ash'   then `xte 'key w'`
+      when 'ash'   then `xte 'key x'`
+      when 'ash'   then `xte 'key y'`
+      when 'ash'   then `xte 'key z'`
+      when 'one'   then `echo 'awful.tag.viewonly(tags[mouse.screen][1])' | awesome-client`
+      when '1'   then `echo 'awful.tag.viewonly(tags[mouse.screen][1])' | awesome-client`
+      when 'two'   then `echo 'awful.tag.viewonly(tags[mouse.screen][2])' | awesome-client`
+      when '2'   then `echo 'awful.tag.viewonly(tags[mouse.screen][2])' | awesome-client`
+      when 'three'   then `echo 'awful.tag.viewonly(tags[mouse.screen][3])' | awesome-client`
+      when '3'   then `echo 'awful.tag.viewonly(tags[mouse.screen][3])' | awesome-client`
+      when 'seven'   then `echo 'awful.tag.viewonly(tags[mouse.screen][7])' | awesome-client`
+      when '7'   then `echo 'awful.tag.viewonly(tags[mouse.screen][7])' | awesome-client`
+      when 'right' then `xte 'mousermove 50 0'`
+      when 'left'  then `xte 'mousermove -50 0'`
+      when 'down'  then `xte 'mousermove 0 50'`
+      when 'up'    then `xte 'mousermove 0 -50'`
+      when 'click' then `xte 'mouseclick 1'`
+      else
+        puts "no command #{@command}"
+      end
+    rescue StandardError => e
+      p e
+    end
+
     def sys_notify message
       `notify-send #{message}`
     end
   end
-
 end
 
 
 puts 'Ready.'
 Root.run
-#Root.run if __FILE__ == $0
 #system 'stty -raw echo'
