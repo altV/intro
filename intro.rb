e097126b0cdeae7286a62cc1e35db87be0086a32
Leo <leonid.inbox@gmail.com>
2013-05-06 21:56:24 +0400

    before sockets

--- intro.rb
+++ intro.rb
@@ -42,7 +42,8 @@ module Root
         @command = @command[-1] if @command[0] == "\x7F"
         @context = []
         puts "Context: #{@command}"
-        ($ninja=1; break) if @command == 'ninja'
+        #Basic.notify @command
+        Basic.process @command
       end
     end
   end
@@ -51,44 +52,85 @@ module Root
     extend self
 
     def process command
+      words = command.split(' ')
       case command
       when 'browser' then `google-chrome`
-      when 'greet'   then `notify-send ohaaaaaaai`
+      when /^ruby .+/
+        begin
+          eval "#{words[1]} '#{words[2..-1].join(' ')}'"
+        rescue Exception => e
+          p e
+        end
+      when /^say .+/        then `xte 'str #{words[1..-1].join(' ')}'`
+      when /^phrase .+/
+        `xte 'str #{words[1..-1].join(' ')}'`
+        `xte 'key Return' ')}'`
 
-      when 'ash'   then `xte 'key a'`
-      when 'ash'   then `xte 'key b'`
-      when 'ash'   then `xte 'key c'`
-      when 'ash'   then `xte 'key d'`
-      when 'ash'   then `xte 'key e'`
-      when 'ash'   then `xte 'key f'`
-      when 'ash'   then `xte 'key g'`
-      when 'ash'   then `xte 'key h'`
-      when 'ash'   then `xte 'key i'`
-      when 'ash'   then `xte 'key j'`
-      when 'ash'   then `xte 'key k'`
-      when 'ash'   then `xte 'key l'`
-      when 'ash'   then `xte 'key m'`
-      when 'ash'   then `xte 'key n'`
-      when 'ash'   then `xte 'key o'`
-      when 'ash'   then `xte 'key p'`
-      when 'ash'   then `xte 'key q'`
-      when 'ash'   then `xte 'key r'`
-      when 'ash'   then `xte 'key s'`
-      when 'ash'   then `xte 'key t'`
-      when 'ash'   then `xte 'key u'`
-      when 'ash'   then `xte 'key v'`
-      when 'ash'   then `xte 'key w'`
-      when 'ash'   then `xte 'key x'`
-      when 'ash'   then `xte 'key y'`
-      when 'ash'   then `xte 'key z'`
-      when 'one'   then `echo 'awful.tag.viewonly(tags[mouse.screen][1])' | awesome-client`
-      when '1'   then `echo 'awful.tag.viewonly(tags[mouse.screen][1])' | awesome-client`
-      when 'two'   then `echo 'awful.tag.viewonly(tags[mouse.screen][2])' | awesome-client`
-      when '2'   then `echo 'awful.tag.viewonly(tags[mouse.screen][2])' | awesome-client`
-      when 'three'   then `echo 'awful.tag.viewonly(tags[mouse.screen][3])' | awesome-client`
-      when '3'   then `echo 'awful.tag.viewonly(tags[mouse.screen][3])' | awesome-client`
-      when 'seven'   then `echo 'awful.tag.viewonly(tags[mouse.screen][7])' | awesome-client`
-      when '7'   then `echo 'awful.tag.viewonly(tags[mouse.screen][7])' | awesome-client`
+      when 'Aff'            then `xte 'key a'`
+      when 'Brav'           then `xte 'key a'`
+      when 'Cai'            then `xte 'key a'`
+      when 'Doy'            then `xte 'key a'`
+      when 'Eck'            then `xte 'key a'`
+      when 'Fay'            then `xte 'key a'`
+      when 'Goff'           then `xte 'key a'`
+      when 'Hoop'           then `xte 'key a'`
+      when 'Ish'            then `xte 'key a'`
+      when 'Jo'             then `xte 'key a'`
+      when 'Keel'           then `xte 'key a'`
+      when 'Lee'            then `xte 'key a'`
+      when 'Mike'           then `xte 'key a'`
+      when 'Noy'            then `xte 'key a'`
+      when 'Osc','Osh'      then `xte 'key a'`
+      when 'Pui','Pom'      then `xte 'key a'`
+      when 'Quebec','Queen' then `xte 'key a'`
+      when 'Ree'            then `xte 'key a'`
+      when 'Soi'            then `xte 'key a'`
+      when 'Tay'            then `xte 'key a'`
+      when 'Uni','Umm'      then `xte 'key a'`
+      when 'Van'            then `xte 'key a'`
+      when 'Wes'            then `xte 'key a'`
+      when 'Xanth'          then `xte 'key a'`
+      when 'Yaa'            then `xte 'key a'`
+      when 'Zul'            then `xte 'key a'`
+      when 'ash'            then `xte 'key a'`
+      when 'ash'            then `xte 'key b'`
+      when 'ash'            then `xte 'key c'`
+      when 'ash'            then `xte 'key d'`
+      when 'ash'            then `xte 'key e'`
+      when 'ash'            then `xte 'key f'`
+      when 'ash'            then `xte 'key g'`
+      when 'ash'            then `xte 'key h'`
+      when 'ash'            then `xte 'key i'`
+      when 'ash'            then `xte 'key j'`
+      when 'ash'            then `xte 'key k'`
+      when 'ash'            then `xte 'key l'`
+      when 'ash'            then `xte 'key m'`
+      when 'ash'            then `xte 'key n'`
+      when 'ash'            then `xte 'key o'`
+      when 'ash'            then `xte 'key p'`
+      when 'ash'            then `xte 'key q'`
+      when 'ash'            then `xte 'key r'`
+      when 'ash'            then `xte 'key s'`
+      when 'ash'            then `xte 'key t'`
+      when 'ash'            then `xte 'key u'`
+      when 'ash'            then `xte 'key v'`
+      when 'ash'            then `xte 'key w'`
+      when 'ash'            then `xte 'key x'`
+      when 'ash'            then `xte 'key y'`
+      when 'ash'            then `xte 'key z'`
+
+      when 'enter'          then `xte 'key Return'`
+
+      when 'one','1'   then awesome 'awful.tag.viewonly(tags[mouse.screen][1])'
+      when 'two','2'   then awesome 'awful.tag.viewonly(tags[mouse.screen][2])'
+      when 'three','3' then awesome 'awful.tag.viewonly(tags[mouse.screen][3])'
+      when 'four','4'  then awesome 'awful.tag.viewonly(tags[mouse.screen][4])'
+      when 'five','5'  then awesome 'awful.tag.viewonly(tags[mouse.screen][5])'
+      when 'six','6'   then awesome 'awful.tag.viewonly(tags[mouse.screen][6])'
+      when 'seven','7' then awesome 'awful.tag.viewonly(tags[mouse.screen][7])'
+      when 'eight','8' then awesome 'awful.tag.viewonly(tags[mouse.screen][8])'
+      when 'nine','9'  then awesome 'awful.tag.viewonly(tags[mouse.screen][9])'
+
       when 'right' then `xte 'mousermove 50 0'`
       when 'left'  then `xte 'mousermove -50 0'`
       when 'down'  then `xte 'mousermove 0 50'`
@@ -101,9 +143,11 @@ module Root
       p e
     end
 
-    def sys_notify message
-      `notify-send #{message}`
-    end
+    def awesome     str; `echo "return #{str}" | awesome-client`                          end
+    def awesome_say str; `echo "return #{str.gsub!('"','\"')}" | awesome-client | espeak` end
+
+    def notify message; `notify-send "Dragon" "#{message.gsub!('"','\"')}"`               end
+    def say    message; `echo "#{message.gsub!('"','\"')}" | espeak`                      end
   end
 end
 
